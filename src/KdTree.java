282a489a2fedaf989ac91daa8c40e0cff186686e
fannarl <12133432+fannarl@users.noreply.github.com>
2021-10-11 02:00:14 +0000

    range find partial solution

--- src/KdTree.java
+++ src/KdTree.java
@@ -2,8 +2,8 @@
 /*************************************************************************
  *************************************************************************/
 
-import java.awt.*;
-import java.util.Arrays;
+import java.util.*;
+import java.util.Queue;
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.In;
@@ -23,10 +23,11 @@ public class KdTree {
         private Node left;
         private Node right;
 
-        public Node(Point2D p){
+        public Node(Point2D p, RectHV rect){
             this.left = null;
             this.right = null;
             this.p = p;
+            this.rect = rect;
         }
 
         public boolean tree_check(Point2D p, boolean vertical){
@@ -58,34 +59,45 @@ public class KdTree {
     public void insert(Point2D p) {
         if(isEmpty()){
             size += 1;
-            root = new Node(p);
+            root = new Node(p, new RectHV(0, 0,1,1));
         }
         else{
-            insert(root, p, false);
+            insert(root, p, false, 0.0, 0.0, 1.0,1.0);
         }
     };
 
-    private void insert(Node x, Point2D p, boolean vertical){
+    private Node insert(Node x, Point2D p, boolean vertical, double xmin, double ymin, double xmax, double ymax){
+        // Insert when node is empty
+        if (x == null){
+            size++;
+            RectHV rect = new RectHV(xmin, ymin, xmax, ymax);
+            return new Node(p, rect);
+        }
+        // If the Node is not equal to one and other.
         if(!p.equals(x.p)){
+            // if the current node is vertical
             if(x.tree_check(p, vertical)){
-                if(x.left == null){
-                    size += 1;
-                    x.left = new Node(p);
+                // compare the x-coordinates
+                double compare = p.x() - x.p.x();
+                if(compare < 0){
+                    x.left = insert(x.left, p, !vertical, xmin, ymin, x.p.x(), ymax);
                 }
                 else{
-                    insert(x.left, p, !vertical);
+                    x. right = insert(x.right, p, !vertical, x.p.x(), ymin, xmax, ymax);
                 }
             }
+            // Current not is horizontal so we compare the y-coordinates
             else{
-                if(x.right == null){
-                    size += 1;
-                    x.right = new Node(p);
+                double compare = p.y() - x.p.y();
+                if(compare < 0){
+                    x.left = insert(x.left, p, !vertical, xmin, ymin, xmax, x.p.y());
                 }
                 else{
-                    insert(x.right, p, !vertical);
+                    x.right = insert(x.right, p, !vertical, xmin, x.p.y(), xmax, ymax);
                 }
             }
         }
+        return x;
     }
 
     // does the set contain the point p?
@@ -135,7 +147,22 @@ public class KdTree {
     }
     // all points in the set that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        List<Point2D> rangeList = new ArrayList<>();
+        range(root, rect, rangeList);
+        return rangeList;
+    }
+
+    // Find the points the lie within the rectangle
+    private void range(Node x, RectHV rect, List<Point2D> rangeList){
+        if (x == null){ return; }
+
+        if(x.rect.intersects(rect)){
+            if(rect.contains(x.p)){
+                rangeList.add(x.p);
+            }
+            range(x.left, rect, rangeList);
+            range(x.right, rect, rangeList);
+        }
     }
 
     // a nearest neighbor in the set to p; null if set is empty
@@ -149,54 +176,33 @@ public class KdTree {
     public static void main(String[] args) {
         In in = new In();
         Out out = new Out();
-        int nrOfRecangles = in.readInt();
-        int nrOfPointsCont = in.readInt();
-        int nrOfPointsNear = in.readInt();
-        RectHV[] rectangles = new RectHV[nrOfRecangles];
-        Point2D[] pointsCont = new Point2D[nrOfPointsCont];
-        Point2D[] pointsNear = new Point2D[nrOfPointsNear];
-        for (int i = 0; i < nrOfRecangles; i++) {
+        int N = in.readInt(), R = in.readInt(), T = 800;
+        RectHV[] rectangles = new RectHV[R];
+        KdTree tree = new KdTree();
+        out.printf("Inserting %d points into tree\n", N);
+        for (int i = 0; i < N; i++) {
+            tree.insert(new Point2D(in.readDouble(), in.readDouble()));
+        }
+        out.printf("tree.size(): %d\n", tree.size());
+        out.printf("Testing `range` method, querying %d rectangles\n", R);
+        ArrayList<Point2D> range = new ArrayList<Point2D>();
+        for (int i = 0; i < R; i++) {
             rectangles[i] = new RectHV(in.readDouble(), in.readDouble(),
                     in.readDouble(), in.readDouble());
-        }
-        for (int i = 0; i < nrOfPointsCont; i++) {
-            pointsCont[i] = new Point2D(in.readDouble(), in.readDouble());
-        }
-        for (int i = 0; i < nrOfPointsNear; i++) {
-            pointsNear[i] = new Point2D(in.readDouble(), in.readDouble());
-        }
-        KdTree set = new KdTree();
-        for (int i = 0; !in.isEmpty(); i++) {
-            double x = in.readDouble(), y = in.readDouble();
-            set.insert(new Point2D(x, y));
-        }
-        for (int i = 0; i < nrOfRecangles; i++) {
-            // Query on rectangle i, sort the result, and print
-            Iterable<Point2D> ptset = set.range(rectangles[i]);
-            int ptcount = 0;
-            for (Point2D p : ptset)
-                ptcount++;
-            Point2D[] ptarr = new Point2D[ptcount];
-            int j = 0;
-            for (Point2D p : ptset) {
-                ptarr[j] = p;
-                j++;
+            out.printf("Points inside rectangle %s\n", rectangles[i]);
+            for (Point2D point : tree.range(rectangles[i])) {
+                range.add(point);
             }
-            Arrays.sort(ptarr);
-            out.println("Inside rectangle " + (i + 1) + ":");
-            for (j = 0; j < ptcount; j++)
-                out.println(ptarr[j]);
+            Collections.sort(range);
+            for (Point2D point : range) {
+                out.printf("%s\n", point);
+            }
+            range.clear();
         }
-        out.println("Contain test:");
-        for (int i = 0; i < nrOfPointsCont; i++) {
-            out.println((i + 1) + ": " + set.contains(pointsCont[i]));
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < rectangles.length; j++) {
+                tree.range(rectangles[j]);
+            }
         }
-
-        out.println("Nearest test:");
-        for (int i = 0; i < nrOfPointsNear; i++) {
-            out.println((i + 1) + ": " + set.nearest(pointsNear[i]));
-        }
-
-        out.println();
     }
 }
